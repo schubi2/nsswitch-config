d841b70f9cf846fd4a62637b8fcc8db5a886fd0f
Stefan Schubert <schubi@suse.de>
2025-11-20 16:00:38 +0100

    Replaced --admin_changed by --is_generated

--- tests/tst-nsswitch8.sh
+++ tests/tst-nsswitch8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# check option --admin_changed
+# check option +--is_generated
 
 nsswitch_config_exe="$PWD/../nsswitch-config"
 
@@ -20,17 +20,17 @@ if ! [[ -d "$nsswitch_config_root" ]]; then
     fi
 fi
 
-echo $nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin --admin_changed
-$nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin --admin_changed
-if [ $? -eq 1 ]; then
-    echo "Should return 0 and not 1"
+echo $nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin --is_generated
+$nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin --is_generated
+if [ $? -eq 0 ]; then
+    echo "Should return 1 and not 0"
     exit 1
 fi
 
-echo $nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.gen --admin_changed
-$nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin.gen --admin_changed
-if [ $? -eq 0 ]; then
-    echo "Should return 1 and not 0"
+echo $nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.gen --is_generated
+$nsswitch_config_exe --output=$nsswitch_config_root/etc/nsswitch.conf.admin.gen --is_generated
+if [ $? -eq 1 ]; then
+    echo "Should return 0 and not 1"
     exit 1
 fi
 
